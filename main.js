255a5191ba056a6bd9c580211f5008caed3800ab
rodrigo <rodjle@gmail.com>
2022-10-13 17:01:43 -0300

    ajuste porta

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ var express=require("express");
 var bodyParser = require('body-parser')
 //framework express
 var app=express();
-const PORT=8000;
+// 3000 is an example port
+const port = process.env.PORT || 8000
 //express-handlebars - https://github.com/ericf/express-handlebars
 const { engine }   = require('express-handlebars');
 // define a extensão e a instância do handlebars com o 
